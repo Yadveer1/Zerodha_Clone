35432e0398685fd5b063d07a6f3b1f6b49093bf3
Yadveer1 <134469422+Yadveer1@users.noreply.github.com>
2025-04-20 23:42:06 +0530

    Home and About Complete

--- src/landing_page/about/AboutPage.jsx
+++ src/landing_page/about/AboutPage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function AboutPage() {
     return ( 
-        <div className="container" style={{margin}}>
+        <div className="container" style={{marginTop: '17vmin'}}>
             <div className="row">
                 <div className="col-4">
-
+                    <img style={{borderRadius:'50%',width: '100%'}} src="assets/images/Yadveer.jpg" alt="" />
                 </div>
-                <div className="col-8">
-                    <p>
+                <div className="col-8 ">
+                    <p className='p-4'>
                         Hey! I'm Yadveer Singh Pawar, a Computer Engineering student with a strong passion for Web Development and Artificial Intelligence. I love turning ideas into real-world projects — like the Spotify clone and Wanderlust (my Airbnb-inspired web app). <br /> <br />
                         Right now, I'm sharpening my skills in Data Structures & Algorithms and diving deeper into full-stack web development. I'm always open to learning, building, and collaborating on meaningful tech projects. <br /> <br />
                         If you're looking for someone to join your team, internship, or just vibe over a cool side project — let’s connect! <br /> <br />
